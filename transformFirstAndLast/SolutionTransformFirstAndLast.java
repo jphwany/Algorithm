15bd4e56ebb087f6453dfe2405820fd6b15fee96
jphwany <pjh5762@naver.com>
2022-07-02 01:52:10 +0900

    transformFirstAndLast. 입력받은 배열 첫 요소와 마지막 요소를 키, 값으로 하는 HashMap 리턴 (주석 추가)

--- transformFirstAndLast/SolutionTransformFirstAndLast.java
+++ transformFirstAndLast/SolutionTransformFirstAndLast.java
@@ -1,17 +1,30 @@
+/*
+    Q) 배열을 입력받아서 차례대로 배열 첫 요소, 마지막요소를 키와 값으로 하는 HashMap 리턴
+
+    input) args_1 : arr(String 타입을 요소로 가지는 배열)
+    output) HashMap<String, String> 타입 리턴
+
+    [caution]
+    1. 빈 배열을 입력받을 경우 null 리턴
+    2. 입력으로 받는 배열은 수정하지 않아야함
+
+*/
 import java.util.*;
-
 public class SolutionTransformFirstAndLast {
-
     public HashMap<String, String> transformFirstAndLast(String[] arr){
+        // 빈 배열 입력받을 경우 null 리턴
         if(arr.length == 0){
             return null;
         }
-
+        // 리턴할 HashMap 선언
         HashMap<String, String> map = new HashMap<>();
 
+        // key1에 배열 0번째요소 할당
+        // value1에 배열 마지막 요소 할당 (arr.length -1 은 전체 배열 길이에서 바로 이전이니까 마지막요소이다)
         String key1 = arr[0];
         String value1 = arr[arr.length-1];
 
+        // 선언해놓은 HashMap에 배열 요소 할당해놓은 key1, value1을 put
         map.put(key1, value1);
 
         return map;
--- transformFirstAndLast/TransformFirstAndLast.java
+++ transformFirstAndLast/TransformFirstAndLast.java
@@ -1,3 +1,7 @@
+/*
+    테스트 케이스
+*/
+
 import java.util.*;
 
 public class TransformFirstAndLast {
@@ -8,13 +12,25 @@ public class TransformFirstAndLast {
         String[] arr1 = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
         String[] arr2 = new String[]{"Kevin", "Love", "Hart", "Costner", "Spacey"};
         String[] arr3 = new String[]{"jphwany", "950523", "backend", "dev"};
+        String[] arr4 = new String[]{};
 
         HashMap<String, String> result1 = test.transformFirstAndLast(arr1);
         HashMap<String, String> result2 = test.transformFirstAndLast(arr2);
         HashMap<String, String> result3 = test.transformFirstAndLast(arr3);
+        HashMap<String, String> result4 = test.transformFirstAndLast(arr4);
 
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
+        System.out.println(result4);
     }
 }
+/*
+    [output]
+
+    {Queen=Beyonce}
+    {Kevin=Spacey}
+    {jphwany=dev}
+    null
+
+*/
